8aa8a58df2ed60993dfb503d9aae6dee2a794c76
babul101 <babuldeb101@gmail.com>
2024-03-15 13:49:08 +0530

    adding the media query

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
-const sections = document.querySelectorAll('section')
+const sections = document.querySelectorAll("section");
 
 // Intersection Observer API
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if(entry.intersectionRatio > 0.1){
-            entry.target.classList.add('in-view')
-        }else {
-            entry.target.classList.remove('in-view')
-        }
-    })
-},{
-    threshold:[0.0,0.1,1.0]
-})
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0.1) {
+        entry.target.classList.add("in-view");
+      } else {
+        entry.target.classList.remove("in-view");
+      }
+    });
+  },
+  {
+    threshold: [0.0, 0.1, 1.0],
+  }
+);
 
-sections.forEach(section => {
-    observer.observe(section)
+sections.forEach((section) => {
+  observer.observe(section);
 
-    const div = section.querySelector('div')
-    const mq = window.matchMedia("(prefers-reduced-motion: no-preference)")
-    if(mq.matches){
-
-        document.addEventListener('mousemove',function(e){
-            const aimX = (e.pageX - (window.innerWidth/2))/20
-            const aimY = (e.pageY- (window.innerHeight/2))/-20
-            div.style.transform = `rotateX(${aimY}deg) rotateY(${aimX}deg)`
-        })
-    }
-})
+  const div = section.querySelector("div");
+  const mq = window.matchMedia("(prefers-reduced-motion: no-preference)");
+  if (mq.matches) {
+    document.addEventListener("mousemove", function (e) {
+      const aimX = (e.clientX - window.innerWidth / 2) / 20;
+      const aimY = (e.clientY - window.innerHeight / 2) / -20;
+      div.style.transform = `rotateX(${aimY}deg) rotateY(${aimX}deg)`;
+    });
+  }
+});
